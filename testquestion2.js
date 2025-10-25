bdd651dd34ffe34b420a0ac698b7ea3074d372aa
chialucas <chialucas56@gmail.com>
2025-10-25 15:17:04 +0800

    Update testquestion2.js

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,15 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
+
+function setup(){
+    createCanvas(200, 300);
+    background("pink")
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    
+    yPos = 100;
+}
+
+
 
