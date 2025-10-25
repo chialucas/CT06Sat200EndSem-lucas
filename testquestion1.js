1e2770040353a881cae31a973737bc7c47c308f3
chialucas <chialucas56@gmail.com>
2025-10-25 15:16:49 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,26 @@
 
+
+function setup(){
+    createCanvas(600,400)
+    background("lightblue")
+    PosX = width/ 2;
+    PosY = width/ 2;
+    XSpeed = 5;
+    fill("limegreen");
+}
+
+function draw(){
+    background("lightblue")
+    circle(PosX,PosY-100,50)
+    PosX = PosX + XSpeed;
+    if(PosX-50/2 <= 0 || PosX + 50/2 >= width) {
+        XSpeed = XSpeed * -1;
+
+    }
+    
+
+    circle(PosX, circle)
+}
+
+noStroke();
+fill(255);
